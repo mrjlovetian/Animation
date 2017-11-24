58c2fbd4dd533f304fd542e2258c8fcffdf78317
MRJ <mrjyuhongjiang@gmail.com>
2017-11-24 16:15:18 +0800

    create file

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by Mr on 2017/11/24.
+//  Copyright © 2017年 Mr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by Mr on 2017/11/24.
+//  Copyright © 2017年 Mr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
